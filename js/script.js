6053e26f742386b18ca33be3c62ed2d49268650d
geometrydashmods <104553503+geometrydashmods@users.noreply.github.com>
2022-04-30 14:20:36 +0700

    fixed navbar on left side 🤡

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   sidenavs = document.querySelectorAll('.sidenav')
-  M.Sidenav.init(sidenavs)
+  M.Sidenav.init(sidenavs, {
+    edge: 'right'
+  })
 
   dropdown = document.querySelectorAll('.dropdown-trigger')
   dropdowncontainer = document.querySelector('.dropdown-container')
