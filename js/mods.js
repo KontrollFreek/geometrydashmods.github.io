35b24ec184053cd74243389f8885a5f19264372a
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-05-02 13:13:54 -0400

    Add files via upload

---
+++ js/mods.js
@@ -0,0 +1,25 @@
+const mods = [
+    // image: "The image used for the top of the card. Preferrably 607 x 201."
+    // name: "The name of the mod. Prefferably the name set by the creator."
+    // description: "A description of the mod. Should be short and simple!"
+    {
+        image: "mods-images/saimodpack.png",
+        name: "Sai Mod Pack",
+        description: "A mod pack with many integrated features, such as FPS Changer, color pallette, custom particles, and more!"
+    },
+    {
+        image: "mods-images/betteredit.png",
+        name: "BetterEdit",
+        description: "Improve your editor experience! Makes the GD editor a lot better for the user."
+    },
+    {
+        image: "mods-images/gdshare.png",
+        name: "GDShare",
+        description: "Export your GD levels as a file and import them to another device."
+    },
+    {
+        image: "mods-images/iconsave.png",
+        name: "IconSave",
+        description: "Save other people's icon kits, make your own dream icon kits, and change among them with ease!"
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    $('body').css('overflow', 'auto')
+    $('.loadscreen').fadeOut(300)
+})
+
+for (let i = 0; i < mods.length; i++) {
+    document.getElementById("mods").innerHTML += `<div class="col"><div class="card h-100"><img src="${mods[i].image}" class="card-img-top" alt="..."><div class="card-body bg-dark"><h5 class="card-title text-white">${mods[i].name}</h5><p class="card-text text-muted">${mods[i].description}</p></div></div></div>`
+}
