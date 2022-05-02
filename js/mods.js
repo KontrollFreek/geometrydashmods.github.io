54c29310aea403833572744ff6090fae24ab656e
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-05-02 13:17:09 -0400

    changed mods.js to be a little easier to edit

--- js/mods.js
+++ js/mods.js
@@ -1,24 +1,24 @@
 const mods = [
-    // image: "The image used for the top of the card. Preferrably 607 x 201."
+    // image: "The image used for the top of the card. Preferrably 607 x 201. (Note: The image must be in the mods-images folder)",
     // name: "The name of the mod. Prefferably the name set by the creator."
     // description: "A description of the mod. Should be short and simple!"
     {
-        image: "mods-images/saimodpack.png",
+        image: "saimodpack.png",
         name: "Sai Mod Pack",
         description: "A mod pack with many integrated features, such as FPS Changer, color pallette, custom particles, and more!"
     },
     {
-        image: "mods-images/betteredit.png",
+        image: "betteredit.png",
         name: "BetterEdit",
         description: "Improve your editor experience! Makes the GD editor a lot better for the user."
     },
     {
-        image: "mods-images/gdshare.png",
+        image: "gdshare.png",
         name: "GDShare",
         description: "Export your GD levels as a file and import them to another device."
     },
     {
-        image: "mods-images/iconsave.png",
+        image: "iconsave.png",
         name: "IconSave",
         description: "Save other people's icon kits, make your own dream icon kits, and change among them with ease!"
     },
--- js/script.js
+++ js/script.js
@@ -4,5 +4,5 @@ window.addEventListener('load', function () {
 })
 
 for (let i = 0; i < mods.length; i++) {
-    document.getElementById("mods").innerHTML += `<div class="col"><div class="card h-100"><img src="${mods[i].image}" class="card-img-top" alt="..."><div class="card-body bg-dark"><h5 class="card-title text-white">${mods[i].name}</h5><p class="card-text text-muted">${mods[i].description}</p></div></div></div>`
+    document.getElementById("mods").innerHTML += `<div class="col"><div class="card h-100"><img src="mods-images/${mods[i].image}" class="card-img-top" alt="..."><div class="card-body bg-dark"><h5 class="card-title text-white">${mods[i].name}</h5><p class="card-text text-muted">${mods[i].description}</p></div></div></div>`
 }
